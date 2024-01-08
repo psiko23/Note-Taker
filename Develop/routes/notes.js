8babdf8370c311680ff6268c1a4a3b969b007505
Psiko23 <tylerpittman2001@gmail.com>
2024-01-08 14:10:15 -0800

    public files

---
+++ Develop/routes/notes.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+const path = require('path');
+
+const notesRouter = express.Router();
+
+const filePath = '../Develop/db/db.json';
+
+notesRouter.get('/', (req, res) => {
+    // const filePath = path.join(__dirname, '..db/db.json');
+    console.log(`${req.method} received successfully!`);
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+        if(err) {
+            console.log(`Internal server error: `, err);
+            return
+        } else {
+            data = JSON.parse(data);
+            res.json(data);
+        }
+    })
+})
+
+notesRouter.post('/', (req,res) => {
+    console.log(`${req.method} request received successfully!`);
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+        if (err) {
+            throw new Error('Error reading file');
+        } 
+        
+        const savedNotes = JSON.parse(data);
+        const newNote = req.body;
+        newNote.id = uuidv4();
+        console.log(newNote);
+        savedNotes.push(newNote);
+
+        fs.writeFile(filePath, JSON.stringify(savedNotes), (err) => {
+            if (err) {
+                console.log('error posting note: ', err)
+            }
+            console.log('note saved successfully');
+            res.json(newNote);
+        }); 
+    });
+});
+
+notesRouter.delete('/:id', (req, res) => {
+    console.log(`${req.method} request received`);
+    const noteID = req.params.id;
+    
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+        if (err) {
+            throw new Error('Error reading file');
+        }
+    const savedNotes = JSON.parse(data);
+    // filters the note IDs in search of the note looking to be deleted, then creates a new array of notes
+        // if note.id !== noteID, then the test passes, and note is added to the array
+    const newArr = savedNotes.filter(note => note.id !== noteID);
+    fs.writeFile(filePath, JSON.stringify(newArr), (err) => {
+        if (err) {
+            console.log('error writing note object')
+        }
+        console.log('note deleted successfully');
+        res.json(newArr)
+        })
+    })
+})
+
+module.exports = notesRouter;
--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
-// const api = require('./routes/router.js');
-
 const PORT = process.env.PORT || 3001;
+
+const api = require('./routes/router');
 
 const app = express();
 
@@ -13,7 +12,7 @@ app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended:true }));
 app.use(express.json());
-// app.use('/api', api);
+app.use('/api', api);
 
 
 app.get('/', (req,res) => {
@@ -23,66 +22,6 @@ app.get('/', (req,res) => {
 app.get('/notes', (req,res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 });
-
-app.get('/api/notes', (req, res) => {
-    console.log(`${req.method} received successfully!`);
-    const filePath = path.join(__dirname, 'db/db.json');
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-        if(err) {
-            console.log(`Internal server error: `, err);
-            return
-        } else {
-            data = JSON.parse(data);
-            res.json(data);
-        }
-    })
-})
-
-app.post('/api/notes', (req,res) => {
-    // console.log(`${req.method} request received successfully!`);
-    const filePath = path.join(__dirname, 'db/db.json');
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-        if (err) {
-            throw new Error('Error reading file');
-        } 
-        
-        const savedNotes = JSON.parse(data);
-        const newNote = req.body;
-        newNote.id = uuidv4();
-        console.log(newNote);
-        savedNotes.push(newNote);
-
-        fs.writeFile(filePath, JSON.stringify(savedNotes), (err) => {
-            if (err) {
-                console.log('error posting note: ', err)
-            }
-            console.log('note saved successfully');
-            res.json(newNote);
-        }); 
-    });
-});
-
-app.delete('/api/notes/:id', (req, res) => {
-    console.log(`${req.method} request received`);
-    const noteID = req.params.id;
-
-    const filePath = path.join(__dirname, 'db/db.json');
-    
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-        if (err) {
-            throw new Error('Error reading file');
-        }
-    const savedNotes = JSON.parse(data);
-    const newArr = savedNotes.filter(note => note.id !== noteID);
-    fs.writeFile(filePath, JSON.stringify(newArr), (err) => {
-        if (err) {
-            console.log('error writing note object')
-        }
-        console.log('note deleted successfully');
-        res.json(newArr)
-        })
-    })
-})
 
 app.get('*', (req, res) => {
     res.sendFile((path.join(__dirname, 'public/index.html')), 'utf-8')
